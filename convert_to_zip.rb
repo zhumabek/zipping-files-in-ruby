722a3603d55c16854d3da0c288648f12d0a6ab7e
zhumabek <30982372+zhumabek@users.noreply.github.com>
2020-05-26 22:49:06 +0600

    Basic zip archive creation
    Basic zip archive creation

---
+++ convert_to_zip.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'zip'
+
+folder = "Users/me/Desktop/stuff_to_zip"
+input_filenames = ['image.jpg', 'description.txt', 'stats.csv']
+
+zipfile_name = "/Users/me/Desktop/archive.zip"
+
+Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
+  input_filenames.each do |filename|
+    # Two arguments:
+    # - The name of the file as it will appear in the archive
+    # - The original file, including the path to find it
+    zipfile.add(filename, File.join(folder, filename))
+  end
+  zipfile.get_output_stream("myFile") { |f| f.write "myFile contains just this" }
+end
